9b6a1593b37c296b96e5692e8e76ec027b70cb20
Yashhasiaj71 <166124414+Yashhasiaj71@users.noreply.github.com>
2024-04-22 08:55:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//alert("this is a alert");
+//let name = prompt("what is your name","guest?");
+//console.log (name);
+let deletepost = confirm("do you really want to delete this post?");
+console.log (deletepost);
+if (deletepost){
+    console.log("your post has been deleted");
+}
+else {
+    console.log("the post has not been deleted ");
+}
